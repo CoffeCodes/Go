8b4ce0929f2fb42151f89b9d05c22ed317e00a13
CoffeCodes <whiteteenboy@gmail.com>
2015-10-22 01:16:23 +0630

    Fixed bugs

--- handlers.go
+++ handlers.go
@@ -1,37 +1,30 @@
 package main
-import ( "fmt"
-"encoding/json"
-"net/http"
-"github.com/gorilla/mux"
+
+import (
+    "encoding/json"
+    "fmt"
+    "net/http"
+
+    "github.com/gorilla/mux"
 )
 
-
-func Index(w http.ResponseWriter,r *http.Request){
-
-fmt.Fprintln(w,"!Welcome");
+func Index(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Welcome!")
 }
 
-func TodoIndex(w http.ResponseWriter,r *http.Request){
-todos := Todos {
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+    todos := Todos{
+        Todo{Name: "Write presentation"},
+        Todo{Name: "Host meetup"},
+    }
 
-Todo{Name : "This is Go Lang"}
-Todo{Name : "SO whats?"}
-
-
-}
-if err := json.NewEncoder(w).Encode(todos);err != nil{
-panic(err)
-}
-
-
-}
-func TodoShow(w http.ResponseWriter,r *http.Request){
-vars := mux.Vars(r);
-todoId := vars["todoId"]
-fmt.Fprintln(w,"Todo Show",todo)
-
-
+    if err := json.NewEncoder(w).Encode(todos); err != nil {
+        panic(err)
+    }
 }
 
-
-
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    todoId := vars["todoId"]
+    fmt.Fprintln(w, "Todo show:", todoId)
+}
